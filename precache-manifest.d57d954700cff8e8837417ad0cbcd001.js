97c9b0c8da21b59a6ade9a39b8233b678b8022e7
Ashik Paul <ashik.paul@sacha.group>
2020-05-11 19:07:47 +0530

    Updates

---
+++ precache-manifest.d57d954700cff8e8837417ad0cbcd001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73bad3212802ea8afd9f521cecbf79e",
+    "url": "/Cat-Project/index.html"
+  },
+  {
+    "revision": "3614f95aa17a936bec2d",
+    "url": "/Cat-Project/static/css/main.9e7542aa.chunk.css"
+  },
+  {
+    "revision": "e768ee5a363116bdd0da",
+    "url": "/Cat-Project/static/js/2.2c7f4505.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Cat-Project/static/js/2.2c7f4505.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3614f95aa17a936bec2d",
+    "url": "/Cat-Project/static/js/main.9acc397a.chunk.js"
+  },
+  {
+    "revision": "6f1af732ad8ca5d69a98",
+    "url": "/Cat-Project/static/js/runtime-main.500b4428.js"
+  }
+]);
